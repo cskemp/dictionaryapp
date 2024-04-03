656c17bbabd4ba198956c568f23522f26edbc87a
Charles Kemp <c.kemp@unimelb.edu.au>
2024-04-04 04:26:43 +1100

    updated for current BILA data

--- app.R
+++ app.R
@@ -5,15 +5,17 @@ library(ggrepel)
 library(maps)
 library(sf)
 
-plo <- read_csv(here("data", "anylex_app_stats.csv")) %>%
-  arrange(langname_data) %>%
+# on local machine can use bila_app_stats_6000.csv.gz
+# if so change maxOptions in two places later
+plo <- read_csv(here("data", "bila_app_stats_2000.csv")) %>%
+  arrange(langname) %>%
   arrange(word)
 
-top_thresh <- quantile(plo$log_odds_weighted, probs = c(0.95))
+top_thresh <- quantile(plo$zeta, probs = c(0.95))
 
 posns <- plo %>%
-  select(gcode_data, longitude_data, latitude_data) %>%
-  mutate(longitude_data = if_else(longitude_data > 180, longitude_data - 360, longitude_data)) %>%
+  select(glottocode, longitude, latitude) %>%
+  mutate(longitude = if_else(longitude > 180, longitude - 360, longitude)) %>%
   unique()
 
 world <- map_data("world")
@@ -29,8 +31,8 @@ ui = fluidPage(
     tabPanel("Dictionary", fluid = TRUE,
              fluidRow(
                 column(4,
-                    selectizeInput("word", "Choose a word:", choices = NULL, options= list(maxOptions = 5000)),
-                    selectizeInput("language", "Choose a language:", choices = NULL, options= list(maxOptions = 5000)),
+                    selectizeInput("word", "Choose a word:", choices = NULL, options= list(maxOptions = 2000)),
+                    selectizeInput("language", "Choose a language:", choices = NULL, options= list(maxOptions = 2000)),
                 ),
                 column(8,
                   div(style = "height:350px;",
@@ -54,55 +56,27 @@ ui = fluidPage(
   )
 )
 
-
-# ui = fluidPage(
-#   tabsetPanel(
-#     tabPanel("Dictionary", fluid = TRUE,
-#              sidebarLayout(
-#                 sidebarPanel(
-#                   #uiOutput("word"),
-#                   selectizeInput("word", "Choose a word:", choices = NULL, options= list(maxOptions = 5000)),
-#                   selectizeInput("language", "Choose a language:", choices = NULL, options= list(maxOptions = 5000)),
-#                 ),
-#                mainPanel(
-#                 div(style = "height:350px;",
-#                   plotOutput("lang_strength_plot", height = "100%"),
-#                   plotOutput("lang_strength_plot2", height = "100%")
-#                   )
-#                 )
-#              )
-#     )
-#   )
-# )
-
 server <- function(input, output, session) {
-
-  # Filter the characters based on the selected type
-  data_filtered <- reactive({
-     plo
-  })
-
-
-  observe({updateSelectizeInput(session, 'word', choices = unique(data_filtered()$word), server = TRUE) })
-  observe({updateSelectizeInput(session, 'language', choices = unique(data_filtered()$langname_data), server = TRUE) })
+  observe({updateSelectizeInput(session, 'word', choices = unique(plo$word), server = TRUE) })
+  observe({updateSelectizeInput(session, 'language', choices = unique(plo$langname), server = TRUE) })
 
   output$lang_strength_plot<- renderPlot({
     if (length(input$word) > 0 && input$word != '') { # don't plot anything until a word has been selected
-      selected_char <- data_filtered() %>%
+      selected_char <- plo %>%
         filter(word== input$word) %>%
-        arrange(desc(log_odds_weighted)) %>%
+        arrange(desc(zeta)) %>%
         #head(n=20) %>%
         head(n=10) %>%
-        mutate(langname_data= fct_reorder(langname_data, desc(log_odds_weighted))) %>%
-        mutate(barcolor= if_else(log_odds_weighted > top_thresh, "#F8766D", "grey30"))
+        mutate(langname= fct_reorder(langname, desc(zeta))) %>%
+        mutate(barcolor= if_else(zeta > top_thresh, "#F8766D", "grey30"))
 
-      ggplot(selected_char , aes(x = langname_data, y = log_odds_weighted, label = gcode_data)) +
+      ggplot(selected_char , aes(x = langname, y = zeta, label = glottocode)) +
         #geom_bar(stat="identity", fill = "#F8766D" , color ="#F8766D") +
         scale_fill_identity() +
         scale_color_identity() +
         geom_bar(stat="identity", aes(fill = barcolor, color = barcolor)) +
         #geom_text_repel(y = -1, hjust = "right") +
-        scale_x_discrete(labels = selected_char$langname_data) +
+        scale_x_discrete(labels = selected_char$langname) +
         labs(x = "Language", y = "Score",  title = paste("Top scores for", input$word)) +
         coord_flip()
    }
@@ -111,22 +85,22 @@ server <- function(input, output, session) {
    output$world_map<- renderPlot({
     if (length(input$word) > 0 && input$word!= '') { # don't plot anything until a word has been selected
 
-      focal_posns <- data_filtered() %>%
+      focal_posns <- plo %>%
         filter(word== input$word) %>%
-        arrange(desc(log_odds_weighted)) %>%
+        arrange(desc(zeta)) %>%
         #head(n=20) %>%
-        filter(log_odds_weighted > top_thresh) %>% # top 5
-        select(-latitude_data, -longitude_data) %>%
-        left_join(posns, by = c("gcode_data"))
+        filter(zeta > top_thresh) %>% # top 5
+        select(-latitude, -longitude) %>%
+        left_join(posns, by = c("glottocode"))
 
         ggplot() +
           geom_map(data = world, map = world,
                  aes(long, lat, map_id = region),
                  color = "white", fill = "lightgray", size = 0.1) +
           coord_sf(xlim = c(-180, 180), ylim=c(-55, 80)) +
-          geom_point(data = posns, aes(longitude_data, latitude_data), color = "grey30", size = 1) +
-          #geom_point(data = focal_posns, aes(longitude_data, latitude_data), color = "#F8766D", size = 2) +
-          geom_point(data = focal_posns, aes(longitude_data, latitude_data), color = "red", size = 2) +
+          geom_point(data = posns, aes(longitude, latitude), color = "grey30", size = 1) +
+          ##geom_point(data = focal_posns, aes(longitude, latitude), color = "#F8766D", size = 2) +
+          geom_point(data = focal_posns, aes(longitude, latitude), color = "red", size = 2) +
           theme_void()+
           theme(legend.position = "none")
    }
@@ -134,13 +108,13 @@ server <- function(input, output, session) {
 
     output$lang_strength_plot2<- renderPlot({
     if (length(input$language) > 0 && input$language!= '') { # don't plot anything until a word has been selected
-      selected_char <- data_filtered() %>%
-        filter(langname_data== input$language) %>%
-        arrange(desc(log_odds_weighted)) %>%
+      selected_char <- plo %>%
+        filter(langname== input$language) %>%
+        arrange(desc(zeta)) %>%
         head(n=20) %>%
-        mutate(word= fct_reorder(word, desc(log_odds_weighted)))
+        mutate(word= fct_reorder(word, desc(zeta)))
 
-      ggplot(selected_char , aes(x = word, y = log_odds_weighted, label = word)) +
+      ggplot(selected_char , aes(x = word, y = zeta, label = word)) +
         geom_bar(stat="identity", fill = "#add8e6" , color ="#add8e6") +
         #geom_text_repel(y = -1, hjust = "right") +
         scale_x_discrete(labels = selected_char$word) +
@@ -148,9 +122,6 @@ server <- function(input, output, session) {
         coord_flip()
    }
   })
-
-
-
 }
 
 shinyApp(ui = ui, server = server)
